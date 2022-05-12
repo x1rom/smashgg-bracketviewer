6ab320b219c57fced302a713c538a912a42ee455
Maksim Kurz <kurzmaksim@gmail.com>
2022-05-12 15:45:13 +0200

    Input kept after refresh, cleaned up code

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,9 @@
-var token = '';
-
 const api = "https://api.smash.gg/gql/alpha";
+
+var APIToken = '';
+var slug = '';
+var eventID = '';
+
 $(function(){
     //initialisation
     $('#tournamentInfoTab').addClass("active");
@@ -30,46 +33,64 @@ $(function(){
     //enter API token
     $('#APIToken').on('change', function(){
         token = $('#APIToken').val();
-
+        localStorage.setItem("APIToken", $('#APIToken').val());
     });
+    APIToken = localStorage.getItem("APIToken");
+    $('#APIToken').val(APIToken);
 
     //enter Tournament Slug
     $('#tournamentSlug').on('change', function(){
-        if(token != ''){
+        if(APIToken != ''){
             var APIquery = 'query TournamentQuery($slug:String){tournament(slug: $slug){events{name, id}}}';
             var APIvariables = '{"slug": "' + $('#tournamentSlug').val() + '"}';
-            $.ajax({
-                type: 'POST',
-                url: api,
-                dataType: 'application/json',
-                headers: {Authorization: "Bearer " + token},
-                data: {query: APIquery, variables: APIvariables}
-            }).always(function(data){
-                response = JSON.parse(data.responseText);
+            APIReqest(APIToken, APIquery, APIvariables, function(response){
                 if(response.data.tournament == null){
                     $('#eventSelectorButton').addClass("disabled");
                     $('#eventSelectorButton').removeClass("enabled");
                 }else{
                     $('#eventSelectorButton').addClass("enabled");
                     $('#eventSelectorButton').removeClass("disabled");
-                    $('#eventList').empty();
                     var events = response.data.tournament.events;
-                    events.forEach(event => {
-                        $('#eventList').append('<li><button class="dropdown-item">' + event.name + ' (event id: ' + event.id + ')</button></li>');
-                    });
-                    handleEventButtons();
+                    localStorage.setItem("events", JSON.stringify(events));
+                    handleEventButtons(events);
                 }
-            })
+            });
         }
+        localStorage.setItem("slug", $('#tournamentSlug').val());
     });
-     
+    slug = localStorage.getItem("slug");
+    $('#tournamentSlug').val(slug);
+
+    var events = JSON.parse(localStorage.getItem("events"));
+    if(events != null){
+        $('#eventSelectorButton').addClass("enabled");
+        $('#eventSelectorButton').removeClass("disabled");
+    }
+    handleEventButtons(events);
+    
 });
 
-function handleEventButtons(){
+function handleEventButtons(events){
+    $('#eventList').empty();
+    events.forEach(event => {
+        $('#eventList').append('<li><button class="dropdown-item">' + event.name + ' (event id: ' + event.id + ')</button></li>');
+    });
     $('#eventList').find('button').each(function(){
         this.addEventListener('click', function(){
-
             $('#eventSelectorButton').text($(this).text());
+            
         });
     });
+}
+
+function APIReqest(token, query, vars, func){
+    $.ajax({
+        type: 'POST',
+        url: api,
+        dataType: 'application/json',
+        headers: {Authorization: "Bearer " + token},
+        data: {query: query, variables: vars}
+    }).always(function(data){
+        func(JSON.parse(data.responseText));
+    })
 }
